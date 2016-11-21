67e1cf8cbbbd6c1889d3deaddae89cbee5015e80
Patrick McClory <patrick@mcclory.io>
2016-11-21 11:09:34 -0800

    updating more stuff

--- tests/test_digital_loggers_client.py
+++ tests/test_digital_loggers_client.py
@@ -10,6 +10,7 @@ Tests for `digital_loggers_client` module.
 
 
 import sys
+import os
 import unittest
 from contextlib import contextmanager
 from click.testing import CliRunner
@@ -22,5 +23,12 @@ from digital_loggers_client import cli
 class test_digital_loggers_client(unittest.TestCase):
 
     def setUp(self):
-        client = DigitalLoggers()
+        client = DigitalLoggers(config_file=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/.dl-client.ini'))
         assert True
+
+    def statusCheck(self):
+        client = DigitalLoggers(config_file=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/.dl-client.ini'))
+        assert len(client.status_all()) == client.config.get(port_count, -1)
+
+    def TurnOn(self):
+        client = DigitalLoggers(config_file=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data/.dl-client.ini'))
